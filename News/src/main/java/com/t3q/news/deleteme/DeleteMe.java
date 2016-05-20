6c98e35373646cfea827e3d8fa6a99f96ca6d0d9
byunghoon park <warmpark@t3q.com>
2016-05-20 16:01:35 +0900

    SMS-23 #comment DeleteMe Change.

--- News/src/main/java/com/t3q/news/deleteme/DeleteMe.java
+++ News/src/main/java/com/t3q/news/deleteme/DeleteMe.java
@@ -4,6 +4,11 @@ public class DeleteMe {
 
 	public static void main(String[] args) {
 		System.out.println(" kkkkkkkkk" );
+		System.out.println(" kkkkkkkkk" );
+		System.out.println(" kkkkkkkkk" );
+		System.out.println(" kkkkkkkkk" );
+		System.out.println(" kkkkkkkkk" );
+		System.out.println(" kkkkkkkkk" );
 
 	}
 
